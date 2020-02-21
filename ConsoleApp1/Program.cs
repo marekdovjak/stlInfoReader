dc6b2934d793df306098ca7741870446a8cb4b50
Marek Dovjak <marek.dovjak@taphome.com>
2020-02-21 07:29:26 +0100

    first blood

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currDir = ".";
+
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Pass direcotry to enumerate as first parameter!");
+            }
+            else
+            {
+                currDir = args[0];
+            }
+
+            DirectoryInfo dir = new DirectoryInfo(currDir);
+            StlReader r = new StlReader();
+
+            foreach (var x in dir.EnumerateFiles())
+            {
+                if (!String.Equals(x.Extension, ".stl", StringComparison.CurrentCultureIgnoreCase))
+                    return;
+
+                if (!r.Parse(Path.Combine(currDir, x.FullName)))
+                    return;
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/StlReader.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    struct StlSize
+    {
+        public double X;
+        public double Y;
+        public double Z;
+    }
+    struct StlVertex
+    {
+        public float X;
+        public float Y;
+        public float Z;
+    }
+
+    class StlTriangle
+    {
+        public StlVertex Vector;
+        public StlVertex A, B, C;
+    }
+
+    class StlReader
+    {
+        public StlSize Dimension;
+
+        public bool Parse(string file)
+        {
+            try
+            {
+                using (BinaryReader f = new BinaryReader(File.OpenRead(file)))
+                {
+                    byte[] header = new byte[80];
+                    f.Read(header, 0, header.Length);
+
+                    UInt32 tCount = f.ReadUInt32();
+                    for (uint i = 0; i < tCount; i++)
+                    {
+                        f.Read()
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error parsing file - " + e.Message);
+                return false;
+            }
+
+            return true;
+        }
+
+        private StlVertex ReadVertex(BinaryReader b)
+        {
+            StlVertex s;
+
+            s
+
+            return s;
+        }
+
+        private StlTriangle ReadTriangle(BinaryReader b)
+        {
+            //A = b.ReadSingle();
+            // B = b.ReadSingle();
+            // C = b.ReadSingle();
+        }
+    }
+}
